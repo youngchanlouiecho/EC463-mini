cdd29fd88283c02cae928957318f2dab5dfb7985
Jazmyn Walker <95877548+JAZMYNW@users.noreply.github.com>
2023-09-20 00:21:02 -0400

    Attempted IOS React Native Firebase via tutorial.
     Issues with navigation screen paths not being recognized. Error uploading entire project due to size can email .zip file. Tried troubleshooting navigation errors with chat gpt and stack overflow by checking names and clearing project cache, no luck,

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import LoginScreen from './LoginScreen';
+import RegistrationScreen from './RegistrationScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
